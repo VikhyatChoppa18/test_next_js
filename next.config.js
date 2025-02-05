b5b7a0e4df8867e4b54494a841bb0ebed01e9530
VenkataChoppa18 <venkata_choppa@outlook.com>
2025-02-05 12:11:14 -0500

    next config changes

--- next.config.js
+++ next.config.js
@@ -1,19 +1,3 @@
-const withOptimizedImages = require('next-optimized-images');
-
-module.exports = withOptimizedImages({
-  reactStrictMode: true,
-  /* other config options for optimized images here */
-});
-
 module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/PlayFive/:path*',
-        destination: 'https://www.datainception.co/PlayFive/:path*',
-      },
-      { source: '/UpgradePaywithpaypal/success', destination: '/SuccessPage' },
-      { source: '/UpgradePaywithpaypal/cancel', destination: '/CancelPage' },
-    ];
-  },
+  reactStrictMode: true, // Optional: Enable React Strict Mode
 };
